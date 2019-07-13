8648cc86deacd9c389b5f79d87bdca6cb10a3474
aliu11 <liu.andrewapple90@gmail.com>
2019-07-12 23:31:28 -0700

    Created basic math operator library and a folder to test the methods in library.

--- math_algorithms/math_lib.py
+++ math_algorithms/math_lib.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+# A library containing the math operators. For now, this file will contain all the common math operators. 
+
+def add (var1, var2):
+	return var1 + var2
+
+def subtract(var1, var2): 
+	return var1 - var2
+
+def multiply(var1, var2):
+	return var1 * var2
+
+def divide(var1, var2):
+	return var1/var2 
+
+# 
+def reduce_fraction(fraction):
+	
