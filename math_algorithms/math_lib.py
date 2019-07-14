0a2d33f04357cd625e6f78ef091ed6266cf201ca
Victor <victorhuang6211@gmail.com>
2019-07-13 17:08:37 -0700

    Mathgen - Created polynomial generator and power rule method         - Created math lib test file

--- math_algorithms/math_lib.py
+++ math_algorithms/math_lib.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import random
 
 # A library containing the math operators. For now, this file will contain all the common math operators. 
 
@@ -50,9 +51,100 @@ def reduce_fraction(fraction):
 
 	return numerator + "/" + denominator
 
+#################################################################################
+#
+#  Descrition: This method will generate a simple polynomial expression with
+#    two terms. The minimum and maximum values are set at 1 to 100 for the 
+#    constants and 1 to 10 for the exponents.
+#
+#  Returns simple polynomial with two expressions
+#
+#  Example:
+#
+#################################################################################
+def simple_polygen():
+  return str(random.randint(1,100)) + 'x^' + str(random.randint(1,10)) + '+' + str(random.randint(1,100)) + 'x^' + str(random.randint(1,10))
+  
 
+#################################################################################
+#
+#  Descrition: This method will generate a complex polynomial expression based
+#    off of user input
+#
+#  Params: number of terms, minimum/maximum constant, minimum/maximum exponent
+#
+#  Returns a string of a complex polynomial expression
+#
+#  Example:
+#
+#################################################################################
+def complex_polygen(number_of_terms, min_const=1, max_const=100, min_expo=1, max_expo=10):
+  constants = []
+  exponents = []
+  problem=""
+  for i in range(random.randint(1, number_of_terms)):
+    constants.append(random.randint(min_const, max_const))
+    exponents.append(random.randint(min_expo, max_expo))
+  for i,j in zip(constants, exponents):
+    problem += str(i)
+    problem += 'x^'
+    problem += str(j)
+    problem += '+'
+  problem = problem[:-1]
+  return str(problem)
 
+#################################################################################
+#
+#  Descrition: This method will separate the terms of a polynomial expression 
+#    into an array
+#
+#  Params: polynomial expression
+#
+#  Returns an array of separate terms from the polynomial expression
+#
+#  Example:
+#
+#################################################################################
+def separate_terms(problem):
+  terms = problem.split('+')
+  return terms
 
+#################################################################################
+#
+#  Descrition: This method will solve and return a solution for an inputted 
+#    polynomial expression. The input should be of type string. 
+#  
+#  Format of Input: kx^e
+#    k - constant
+#    x - variable
+#    e - exponent
+#
+#  Returns the derivative of the inputted problem
+#
+#  Example:
+#
+#################################################################################
+def power_rule(problem):
+  terms = separate_terms(problem)
+  constants = []
+  powers = []
+  new_constants = []
+  new_powers = []
+  answer = ""
+  for i in terms:
+    constants.append(i.split('x^', 1)[0])
+    powers.append(i.split('x^', 1)[1])
+  for i, j in zip(constants, powers):
+    new_constants.append(int(i) * int(j))
+  for i in powers:
+    new_powers.append(int(i) - 1)
+  for i,j in zip(new_constants, new_powers):
+    answer += str(i)
+    answer += 'x^'
+    answer += str(j)
+    answer += '+'
+  answer = answer[:-1]
+  return str(answer)
 
 	
 	
---
+++ math_algorithms/math_lib_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.insert(0, './')
+import math_lib
+
+print(math_lib.simple_polygen())
+
+print(math_lib.complex_polygen(5))
+
+print(math_lib.power_rule('5x^2'))
+
