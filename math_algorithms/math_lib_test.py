1a4af3cb9e826fd083f0e0bc037c2d31de214cfe
Victor <victorhuang6211@gmail.com>
2019-07-13 20:41:18 -0700

    MathAlgorithms - Added unittesting to math_lib_test.py                - Added first test case for power rule

--- math_algorithms/math_lib_test.py
+++ math_algorithms/math_lib_test.py
@@ -3,10 +3,20 @@
 import sys
 sys.path.insert(0, './')
 import math_lib
+import unittest
+
+class MathGenTests(unittest.TestCase):
+
+  # Verifies power rule method is working
+  def test_power_rule(self):
+    self.assertEqual(math_lib.power_rule('5x^2'), '10x^1')
+  
 
 print(math_lib.simple_polygen())
 
 print(math_lib.complex_polygen(5))
 
-print(math_lib.power_rule('5x^2'))
+
+if __name__ == '__main__':
+  unittest.main()
 
