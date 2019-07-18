bcb5f71435b20c7bd8c3b007ab35c095f6edae00
aliu11 <liu.andrewapple90@gmail.com>
2019-07-18 11:36:43 -0700

    Created the initializer; currently designing methods

--- math_algorithms/Polynomial.py
+++ math_algorithms/Polynomial.py
@@ -1,5 +1,12 @@
 class Polynomial:
+    # initializer
+    def __init__(self, eq_len, max_deg, min_deg, var_type):
+        self.equation_length = eq_len
+        self.max_degree = max_deg
+        self.min_degree = min_deg
+        self.letter = var_type
 
-	
-	# Still designing polynomial class 
+    # methods
+    def get_poly_function(selfs):
+        pass
 
