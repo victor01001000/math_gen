cf6a02420e9dab2aed6f48c4bc36b5246d897ab9
aliu11 <liu.andrewapple90@gmail.com>
2019-07-18 20:31:22 -0700

    I need to think about the design of the polynomial class.

--- math_algorithms/Polynomial.py
+++ math_algorithms/Polynomial.py
@@ -1,12 +1,3 @@
 class Polynomial:
     # initializer
-    def __init__(self, eq_len, max_deg, min_deg, var_type):
-        self.equation_length = eq_len
-        self.max_degree = max_deg
-        self.min_degree = min_deg
-        self.letter = var_type
-
-    # methods
-    def get_poly_function(selfs):
-        pass
 
