9731ea4dc1c49aa1029e15777a3a007c857c65e9
Victor Huang <victorhuang6211@gmail.com>
2019-07-19 23:14:09 -0700

    MathGen - Created statistics and series classes         - Created data generator tool

---
+++ math_algorithms/data_generator/data_gen.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class DataGen:
+
+  def __init__(self, num_of_values):
+    self.num_of_values =  num_of_values
+
+
---
+++ math_algorithms/math_lib/Series.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class Series
+
+  # Going to finish later
+  def __init__(self, problem):
+    self.problem = problem
+
+  # Checks if series is convergent
+  def convergent():
+  
+
+  # Checks if series is divergent
+  def divergent():
+
+  
+
