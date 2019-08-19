b975357ddda1b96be79391d86982ee5c0015dc7b
Victor Huang <victorhuang6211@gmail.com>
2019-08-18 20:02:12 -0700

    More work on website

--- math_gen/calculus/views.py
+++ math_gen/calculus/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Loads html file for home page
 def home(request):
-  return HttpResponse('<h1>Home</h1>')
+  return render(request, 'calculus/home.html')
 
 def about(request):
   return HttpResponse('<h1>About</h1>')
