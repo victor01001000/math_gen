ff579bab9b116b640464aef8261bed83d00aa37a
aliu11 <37389710+aliu11@users.noreply.github.com>
2019-09-06 17:10:42 -0700

    Updated Polynomial Class design
    I added default values for the parameters of the initializer. I opted for the signs of the coefficient to be randomly assigned instead of relying on the user to include negative numbers in their domain; the domain has to be greater than zero (n < 0). 
    Finally, I added documentation for the initializer.

--- math_algorithms/math_lib/Polynomial.py
+++ math_algorithms/math_lib/Polynomial.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 class Polynomial():
-
-  def __init__(self, min_terms, max_terms, min_const, max_const, min_deg, max_deg, letter):
+  
+  """
+  Initializer has default parameters and can be changed. 
+  
+  Parameters: 
+  -length of equation is a random number between min_terms and max_terms. 
+  -parameters for constants have to be positve. 
+  -parameters for degree can be negative or positive. 
+  -equation is one type of variable letter. 
+  """
+  def __init__(self, min_terms = 1, max_terms = 5, min_const = 1, max_const = 5, min_deg = 0, max_deg = 6, letter = 'x'):
     self.terms_list = list()
    
     for i in range(random.randint(min_terms, max_terms)):
+      signs = ['+', '-']
       self.terms_list.append(
        {
+         'sign': signs[random.randint(0, 1)]
          'constant': random.randint(min_const, max_const),
+         'variable': letter, 
          'exponent': random.randint(min_deg, max_deg),
-         'variable': letter
        }
       )
   
@@ -18,13 +29,14 @@ class Polynomial():
     equation = ''
     
     for i in range(len(self.terms_list)):
-      function = self.terms_list[i]
-      if not (function['constant'] < 0):
-        equation += '+' + str(function['constant']) + function['variable'] + '^' + str(function['exponent']) 
+      gx = self.terms_list[i]
+      equation += gx['signs'] + gx['constant'] + gx['variable'] + '^' + gx['exponent']
       
     return equation
   
   def get_derivative(self):
+    problem_array = self.terms_list
+    
     answer_array = []
     for i in range(len(problem_array)):
       answer_array.append(
@@ -37,17 +49,17 @@ class Polynomial():
       )
     return answer_array
 
-  def generate_equation_sequential(self, min_coeff, max_coeff, min_deg):
+#   def generate_equation_sequential(self, min_coeff, max_coeff, min_deg):
    
-    current_deg = min_deg
-    max_deg = start_deg + self.eq_length
-    while current_deg <= max_deg:
-      coefficient = random.randint(min_coeff, max_coeff)
-      var = Variable(coefficient, self.var_letter, current_deg)  
+#     current_deg = min_deg
+#     max_deg = start_deg + self.eq_length
+#     while current_deg <= max_deg:
+#       coefficient = random.randint(min_coeff, max_coeff)
+#       var = Variable(coefficient, self.var_letter, current_deg)  
       
-      self.terms_list.append(var)
+#       self.terms_list.append(var)
 
-      current_deg = current_deg + 1
+#       current_deg = current_deg + 1
 
-  def generate_equation_random(self, min_coeff, max_coeff, min_deg, max_deg):
-    pass
+#   def generate_equation_random(self, min_coeff, max_coeff, min_deg, max_deg):
+#     pass
