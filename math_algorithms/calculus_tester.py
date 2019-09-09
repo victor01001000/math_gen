02b3c37009e44ac9bd3db91fac9c45ea6af996f6
unknown <anliu2026@gmail.com>
2019-09-08 23:04:52 -0700

    I changed the design of the Polynomial class; I strictly defined the class as a list of monomials. The default parameter for the initializer will be an empty list; the instance variable will be initialized by default values for the parameters of the method generate_random_equation(). I also added a method to generate the equation in string form for testing purposes: get_equation(). In the calculus_tester, I added a test method for finding the derivative of the equation x^2-3x^1, but was not able to run the method; I could properly import the Polynomial class.

--- math_algorithms/calculus_tester.py
+++ math_algorithms/calculus_tester.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python3
-  
+
 import sys
+
 sys.path.insert(0, './math_lib')
 import Polynomial
+import random
 import unittest
+
 
 class PolynomialTests(unittest.TestCase):
 
-poly = Polynomial.Polynomial(1, 5, -3, 10, 2, 8, 'x')
+    def test_get_derivative(self):
+        print('Finding derivative of x^2 + 3x^1')
+        default_terms = []
+        default_terms.append(
+            {
+                'sign': '+',
+                'constant': 1,
+                'variable': 'x',
+                'exponent': 2,
+            }
+        )
 
-print(poly)
-  
+        default_terms.append(
+          {
+            'sign': '-',
+            'constant': 3,
+            'variable': 'x',
+            'exponent': 1,
+          }
+        )
+        
+        function = Polynomial(default_terms)
+        function_prime = Polynomial(function.get_derivative())
+
+        self.assertEqual(function_prime, '2x^1-3x^0')
+        
+        
+
+
 if __name__ == '__main__':
-  unittest.main()
-  
+    unittest.main()
--- math_algorithms/math_lib/Polynomial.py
+++ math_algorithms/math_lib/Polynomial.py
@@ -1,65 +1,108 @@
 #!/usr/bin/env python3
+import random
+
 
 class Polynomial():
-  
-  """
-  Initializer has default parameters and can be changed. 
-  
-  Parameters: 
-  -length of equation is a random number between min_terms and max_terms. 
-  -parameters for constants have to be positve. 
-  -parameters for degree can be negative or positive. 
-  -equation is one type of variable letter. 
-  """
-  def __init__(self, min_terms = 1, max_terms = 5, min_const = 1, max_const = 5, min_deg = 0, max_deg = 6, letter = 'x'):
-    self.terms_list = list()
-   
-    for i in range(random.randint(min_terms, max_terms)):
-      signs = ['+', '-']
-      self.terms_list.append(
-       {
-         'sign': signs[random.randint(0, 1)]
-         'constant': random.randint(min_const, max_const),
-         'variable': letter, 
-         'exponent': random.randint(min_deg, max_deg),
-       }
-      )
-  
-  def __repr__(self):
-    equation = ''
+    """
+    Description:
+    A polynomial is described as a list of monomials.
+
+    The initializer's default parameter is an empty list of monomials; however, it it has
+    defualt parameters to generating a random equation.
+
+    """
+    def __init__(self, terms_list=list()):
+        self.equation = terms_list
+
+        if len(self.equation) is 0:
+            self.equation = list()
+
+            # Default parameters for a random equation
+            min_terms = 1
+            max_terms = 5
+            min_const = 1
+            max_const = 10
+            min_deg = 1
+            max_deg = 7
+
+            self.equation = self.generate_random_equation(min_terms, max_terms, min_const, max_const, min_deg, max_deg)
+
+    def __repr__(self):
+        return self.get_equation()
+
+    """
+    Description: 
+    Generates a random polynomial equation based on the parameters given. 
     
-    for i in range(len(self.terms_list)):
-      gx = self.terms_list[i]
-      equation += gx['signs'] + gx['constant'] + gx['variable'] + '^' + gx['exponent']
+    Parameters:
+    -length of equation is a random number between min_terms and max_terms.
+    -parameters for constants-- min_const and max_const-- have to be positive.
+    -parameters for degree can be negative or positive.
+    -equation is one type of variable letter.
+        
+    Returns: 
+    void
+        
+    """
+    def generate_random_equation(self, min_terms, max_terms, min_const, max_const, min_deg, max_deg, letter):
+        new_equation = list()
+
+        signs = ['+', '-']
+        for i in range(random.randint(min_terms, max_terms)):
+            new_equation.append(
+                {
+                    'sign': signs[random.randint(0, 1)],
+                    'constant': random.randint(min_const, max_const),
+                    'variable': letter,
+                    'exponent': random.randint(min_deg, max_deg),
+                }
+            )
+
+        self.terms_list = new_equation
+
+    """
+    Description: 
+    Generates and returns the first derivative of the equation.
       
-    return equation
-  
-  def get_derivative(self):
-    problem_array = self.terms_list
-    
-    answer_array = []
-    for i in range(len(problem_array)):
-      answer_array.append(
-        {
-          'sign': problem_array[i]['sign'],
-          'constant': problem_array[i]['constant'] * problem_array[i]['exponent'],
-          'exponent': problem_array[i]['exponent'] - 1,
-          'variable': problem_array[i]['variable']
-        }
-      )
-    return answer_array
-
-#   def generate_equation_sequential(self, min_coeff, max_coeff, min_deg):
-   
-#     current_deg = min_deg
-#     max_deg = start_deg + self.eq_length
-#     while current_deg <= max_deg:
-#       coefficient = random.randint(min_coeff, max_coeff)
-#       var = Variable(coefficient, self.var_letter, current_deg)  
+    Parameters:
+    none
       
-#       self.terms_list.append(var)
+    Returns: 
+    A list of dictionaries that define a monomial. 
+    """
+    def get_derivative(self):
+        function = self.equation
 
-#       current_deg = current_deg + 1
+        function_prime = []
+        for i in range(len(function)):
+            function_prime.append(
+                {
+                    'sign': function[i]['sign'],
+                    'constant': function[i]['constant'] * function[i]['exponent'],
+                    'exponent': function[i]['exponent'] - 1,
+                    'variable': function[i]['variable']
+                }
+            )
+        return function_prime
 
-#   def generate_equation_random(self, min_coeff, max_coeff, min_deg, max_deg):
-#     pass
+    """
+        Description: 
+        Constructs a string that represents the equation based on the terms inside self.equation list. 
+        
+        Parameters:
+        None
+        
+        Returns: 
+        The equation constructed in a string. 
+    """
+    def get_equation(self):
+        equation = ''
+
+        for i in range(len(self.equation)):
+            gx = self.equation[i]
+            if (gx['sign'] is '+') and (i == 0):
+                equation += str(gx['constant']) + gx['variable'] + '^' + str(gx['exponent'])
+            else:
+                equation += gx['sign'] + str(gx['constant']) + gx['variable'] + '^' + str(gx['exponent'])
+
+        return equation
